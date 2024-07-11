a7cb654426521cf15063c9b39d1ef3537223fb7e
thaisdev <thaishdzlpz@gmail.com>
2024-07-11 18:27:37 +0100

    Dash streams and choose resolution FAIL

--- src/app.py
+++ src/app.py
@@ -1,52 +1,97 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from functools import partial
-from pytube import YouTube, Stream
+from pytube import YouTube
 from pytube.exceptions import RegexMatchError, AgeRestrictedError
 import threading
+import io
 
 def on_paste(event):
     # Llamamos a check_youtube_link después de 10 milisegundos
     event.widget.after(10, check_youtube_link)
     
 def check_youtube_link():
+    global yt  
     yt_link = app.clipboard_get() 
-    if yt_link:
-        try:   
-            global yt           
-            yt = YouTube(yt_link)
-            choose_resolution_label.pack(pady=20)
-            display_resolutions_radio_button(yt.streams.filter(adaptive=True, file_extension="mp4", only_video=True))
+    try: 
+        yt = YouTube(yt_link)
+        display_resolutions_radio_button()
 
-        except RegexMatchError:
-            messagebox.showerror("Error", message="❌ Invalid YouTube Link, check that it's ok or don't put weird stuff 👺")
-        except AgeRestrictedError:
-            messagebox.showwarning(message="This video is age restricted, please log in to Youtube to download it")
+    except RegexMatchError:
+        messagebox.showerror("Error", message="❌ Invalid YouTube Link ❌")
+        yt = None
+    except AgeRestrictedError:
+        messagebox.showwarning("Warning", message="This video is age restricted, please log in to Youtube to download it")
 
-def display_resolutions_radio_button(streams):
+def display_resolutions_radio_button():
+    choose_resolution_label.pack(pady=20)
     global selected_stream
     selected_stream = tk.StringVar()
-    for stream in streams:
+    for stream in yt.streams.filter(adaptive=True, file_extension="mp4", only_video=True):
         radio_button = tk.Radiobutton(
                                     app, 
                                     text=stream.resolution, 
                                     value=stream.resolution,
                                     variable=selected_stream,
-                                    command=get_selected_stream
+                                    command=get_video_stream
                                     )
         radio_button.pack()
     display_download_vide_button()
 
-def get_selected_stream() :
-    return yt.streams.filter(adaptive=True, file_extension="mp4", only_video=True, res=selected_stream).first()
-
 def display_download_vide_button():
     download_button.pack(ipady=10, pady=20)
-    
-def download_video(): 
 
-    stream = get_selected_stream()
+def get_video_stream():
+    return yt.streams.filter(adaptive=True, file_extension="mp4", only_video=True, res=selected_stream).first()
+
+def update_progress_bar(progress_bar_widget, total_progress, progress_bar_widget_label):
+    progress_bar_widget["value"] = total_progress
+    progress_bar_widget_label.configure(text=f"{total_progress}%")
+    app.update_idletasks()
+
+def download_video_stream(progress_bar_widget, lock, progress_bar_label_widget, shared_data):
+    video_stream = get_video_stream()
+    video_stream.download()
+    filesize = video_stream.filesize
+    bytes_downloaded = 0
+
+    buffer = io.BytesIO()
+    video_stream.stream_to_buffer(buffer=buffer)
+
+    for chunk in buffer.getvalue():
+        bytes_downloaded += len(chunk) # incrementa la cantidad total descargada con el tamaño del chunk actual
+        with lock:
+            percent = (bytes_downloaded / filesize) * 50
+            shared_data["video_progress"] = int(percent)
+            total_progress = shared_data['video_progress'] + shared_data['audio_progress']
+            # Llamar a update_progress en el hilo principal
+            app.after(0, update_progress_bar, progress_bar_widget, total_progress, progress_bar_label_widget)
+    
+
+
+def download_audio_stream(progress, lock, progress_bar_label_widget, shared_data):
+    audio_stream = yt.streams.get_audio_only()
+    filesize = audio_stream.filesize
+    bytes_downloaded = 0
+
+    buffer = io.BytesIO()
+    audio_stream.stream_to_buffer(buffer=buffer)
+
+    for chunk in buffer.getvalue():
+        bytes_downloaded = chunk
+        with lock:
+            percent = (bytes_downloaded / filesize) * 50
+            shared_data["audio_progress"] = int(percent)
+            total_progress = shared_data['video_progress'] + shared_data['audio_progress']
+            # Llamar a update_progress en el hilo principal
+            app.after(0, update_progress_bar, progress, total_progress, progress_bar_label_widget)
+    
+
+    #https://www.youtube.com/shorts/lZ30aycZjFo
+
+def download_video():
+    lock = threading.Lock()
+    shared_data = {'video_progress': 0, 'audio_progress': 0}
 
     # Packing labels
     label_frame.pack(pady=30)
@@ -54,15 +99,11 @@ def download_video():
     downloading_text_label.pack(side=tk.LEFT)
     downloading_percent_label.pack(side=tk.LEFT)
 
-
-    # Register Callback handling by pytube and creating progress bar status thread 
-    progress_bar_status_thread = threading.Thread(yt.register_on_progress_callback(download_progress_bar_status))
-
-    # Create download video thread 
-    download_video_thread = threading.Thread(target=stream.download) 
-
-    download_video_thread.start()
-    progress_bar_status_thread.start()
+    video_thread = threading.Thread(target=download_video_stream, args=(progress_bar, lock, downloading_percent_label, shared_data))
+    audio_thread = threading.Thread(target=download_audio_stream, args=(progress_bar, lock, downloading_percent_label, shared_data))
+    
+    video_thread.start()
+    audio_thread.start()
 
 
 def download_progress_bar_status(stream, chunk, bytes_remaining):
@@ -70,11 +111,11 @@ def download_progress_bar_status(stream, chunk, bytes_remaining):
     bytes_downloaded = total_bytes - bytes_remaining
     percent = (bytes_downloaded / total_bytes) * 100
     progress_bar["value"] = int(percent)
-    text_label = f"{int(percent)}%"
+    text_label = f"{progress_bar["value"]}%"
     downloading_percent_label.configure(text=text_label)
 
     if bytes_remaining == 0:
-        messagebox.showinfo("SUCCESSFULLY", message="Download Complete 🙌")
+        messagebox.showinfo("SUCCESSFULLY", message="Download Complete")
 
     app.update_idletasks()
     
@@ -87,7 +128,7 @@ app.geometry("800x450") # set starting size of window
 app.resizable(False,False) # height, width
 
 # Adding UI elements
-title_box = tk.Label(app, text="Insert a Youtube Link", fg="#fff")
+title_box = tk.Label(app, text="Paste a YouTube Link", fg="#fff")
 title_box.pack(pady=30)
 title_box.config(font=("Font", 30))
 
@@ -101,7 +142,7 @@ yt_link_entry.pack(ipady=5)
 yt_link_entry.bind("<<Paste>>", on_paste)
 
 # searching label
-choose_resolution_label = tk.Label(app, text="Choose a video resolution: ", fg="#fff")
+choose_resolution_label = tk.Label(app, text="Choose a Resolution: ", fg="#fff")
 choose_resolution_label.config(font=("Font", 15))
 
 # Download Button
